4a1d36405e732a2c1de358ae09ff08224f2b7017
eltwenthird <112835123+eltwenthird@users.noreply.github.com>
2023-12-07 00:24:38 +0700

    Delete title-landing.php

--- title-landing.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<html lang="en">
-<head>
-  <title>Toko Online Batik</title>
-  <link rel="icon" type="image/gif/png" href="asset/img/Title.png">
